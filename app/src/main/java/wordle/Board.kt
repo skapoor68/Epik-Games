b9abc69311a90d78e2eaf8c122d2bc635e4981ba
kmohindra354 <91097608+kmohindra354@users.noreply.github.com>
2022-06-16 13:59:42 -0400

    Revert "Fixed last tile bug and added test for it"

--- app/src/main/java/wordle/Board.kt
+++ app/src/main/java/wordle/Board.kt
@@ -20,6 +20,7 @@ class Board(val tileArray: Array<Tile> = Array(HEIGHT * WIDTH) {i -> Tile(i)}, v
             guessWord += tileArray[startTile].char.toString()
             startTile++
         }
+        println(guessWord)
         startTile = curTile - 4
 
         //Have two arrays that will store the occurrences of characters in the guess word and actual word
@@ -58,7 +59,7 @@ class Board(val tileArray: Array<Tile> = Array(HEIGHT * WIDTH) {i -> Tile(i)}, v
             curTile++
         }
         
-        if (curTile > tileArray.size - 1) {
+        if (curTile >= tileArray.size - 1) {
             return
         }
 
