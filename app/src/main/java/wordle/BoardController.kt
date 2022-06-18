96289929152e19d1c1d3950d52934bfe0c2605d5
skapoor68 <skapoor68@gatech.edu>
2022-06-18 17:22:17 -0400

    change how a random word is selected

--- app/src/main/java/wordle/BoardController.kt
+++ app/src/main/java/wordle/BoardController.kt
@@ -1,13 +1,16 @@
 package wordle
 
 import WordsList
+import kotlin.random.Random
 
 
 class BoardController() {
     private val words = WordsList.values()
 
     fun getRandWord(): String {
-        return words.toList().shuffled().first().toString().uppercase()
+        //return words.toList().shuffled().first().toString().uppercase()
+        val index: Int = Random.nextInt(words.size)
+        return words[index].toString().uppercase()
     }
 
     fun getWords(): Array<WordsList> {
