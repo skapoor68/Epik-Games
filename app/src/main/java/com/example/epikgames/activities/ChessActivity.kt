27e82b018fdcc77535a94daae44eddc8e0be8330
sanath24 <sanathkamath24@gmail.com>
2022-06-25 13:45:02 -0400

    added a chess library

--- app/src/main/java/com/example/epikgames/activities/ChessActivity.kt
+++ app/src/main/java/com/example/epikgames/activities/ChessActivity.kt
@@ -3,10 +3,14 @@ package com.example.epikgames.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.epikgames.R
+import com.github.bhlangonijr.chesslib.Board
 
 class ChessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chess)
+
+        val board: Board = Board()
+        println(board.toString())
     }
 }
