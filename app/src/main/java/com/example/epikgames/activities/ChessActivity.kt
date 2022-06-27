81c6b233c290e15c7e968a83619ad63d8602cac3
joeyorofino <josephorofino23@gmail.com>
2022-06-27 12:48:10 -0400

    added functionality

--- app/src/main/java/com/example/epikgames/activities/ChessActivity.kt
+++ app/src/main/java/com/example/epikgames/activities/ChessActivity.kt
@@ -1,12 +1,21 @@
 package com.example.epikgames.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.epikgames.R
 
 class ChessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chess)
+
+        //exit button
+        val exitButton = findViewById<ImageButton>(R.id.chessExitButton)
+        exitButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
