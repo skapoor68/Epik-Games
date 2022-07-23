70fe1d899d6e30150daf6767c02db36eb7f5f4e2
gmridul007 <73621660+gmridul007@users.noreply.github.com>
2022-07-23 00:27:38 -0400

    controller object and popup functionality working
    I made some test objects in the activity class which can be removed later.

--- app/src/main/java/com/example/epikgames/activities/BlackJackActivity.kt
+++ app/src/main/java/com/example/epikgames/activities/BlackJackActivity.kt
@@ -1,6 +1,7 @@
 package com.example.epikgames.activities
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,13 +10,25 @@ import androidx.appcompat.app.AlertDialog
 import com.example.epikgames.R
 
 import blackjack.CardImages
+import blackjack.Game
+import blackjack.GameController
+import blackjack.Player
+import chess.ChessController
+import com.github.bhlangonijr.chesslib.Board
 
 class BlackJackActivity : AppCompatActivity() {
+
+    var controller: GameController = GameController()
+    var player1: Player = Player("Some rando") //can be modified
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blackjack)
 
         startGame()
+
+        //This can be changed
+        controller.initGame("Some ranoo")
 
         val rulesButton = findViewById<ImageButton>(R.id.blackjackRules)
         rulesButton.setOnClickListener {
@@ -31,6 +44,8 @@ class BlackJackActivity : AppCompatActivity() {
 
         val betButton = findViewById<Button>(R.id.betButton)
         betButton.setOnClickListener {
+
+            var betAmt = 0
             val alert: AlertDialog.Builder = AlertDialog.Builder(this)
             val dialogView: View = layoutInflater.inflate(R.layout.betting_popup, null)
             alert.setView(dialogView)
@@ -39,21 +54,46 @@ class BlackJackActivity : AppCompatActivity() {
             betView.textSize = 25F
             betView.text = "PLACE BETS"
 
+            val betAmountText = dialogView.findViewById<TextView>(R.id.betPopUpAmount)
+
+            val bet2: ImageView = dialogView.findViewById(R.id.chip2)
+            bet2.setOnClickListener {
+                betAmt += 2
+                betAmountText.text = "Bet Amount: ".plus(betAmt)
+            }
+
+            val bet10: ImageView = dialogView.findViewById(R.id.chip10)
+            bet10.setOnClickListener {
+                betAmt += 10
+                betAmountText.text = "Bet Amount: ".plus(betAmt)
+            }
+
+            val bet25: ImageView = dialogView.findViewById(R.id.chip25)
+            bet25.setOnClickListener {
+                betAmt += 25
+                betAmountText.text = "Bet Amount: ".plus(betAmt)
+            }
+
+            val bet100: ImageView = dialogView.findViewById(R.id.chip100)
+            bet100.setOnClickListener {
+                betAmt += 100
+                betAmountText.text = "Bet Amount: ".plus(betAmt)
+            }
             //place bet button on the alert
             val placeBet: Button = dialogView.findViewById(R.id.place_bet)
             placeBet.setOnClickListener {
+
+                controller.placeBet(player1, betAmt)
                 val intent = Intent(this,
-                    ChessActivity::class.java)
-
-
+                    BlackJackActivity::class.java)
+                startActivity(intent)
             }
 
             //clear button on the alert
             val clear: Button = dialogView.findViewById(R.id.clear)
             clear.setOnClickListener {
-                //TODO
-                val intent = Intent(this,
-                    MainActivity::class.java)
+                betAmt = 0;
+                betAmountText.text = "Bet Amount: ".plus(betAmt)
             }
 
             //quit(go back) button on the alert
