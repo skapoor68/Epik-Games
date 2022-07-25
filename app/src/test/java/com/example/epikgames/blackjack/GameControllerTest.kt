4f8957e9dc09170d454600964980471ee85471d1
gmridul007 <73621660+gmridul007@users.noreply.github.com>
2022-07-24 23:17:05 -0400

    2 new test cases

--- app/src/test/java/com/example/epikgames/blackjack/GameControllerTest.kt
+++ app/src/test/java/com/example/epikgames/blackjack/GameControllerTest.kt
@@ -44,4 +44,36 @@ class GameControllerTest {
         assertEquals(true, game.players[0].roundOver)
         assertEquals("Player 2", game.getCurrentPlayer()?.name)
     }
+
+    @Test
+    fun roundOverForPlayers() {
+        val game = controller.initGame("Player 1", "Player 2", "Player 3")
+
+        for (player in game.players) {
+            controller.placeBet(player, 10)
+        }
+        controller.roundOverForPlayers(game)
+
+        assertEquals(false, game.players[0].roundOver)
+        assertEquals(false, game.players[1].roundOver)
+        assertEquals(false, game.players[2].roundOver)
+
+        controller.stand(game)
+
+        assertEquals(true, game.players[0].roundOver)
+    }
+
+    @Test
+    fun hit() {
+        val game = controller.initGame("Player 1", "Player 2", "Player 3")
+
+        val transitionQueue: Queue<GameTransition> = LinkedList()
+        for (player in game.players) {
+            controller.placeBet(player, 10)
+        }
+        controller.hit(game, transitionQueue)
+        assertEquals(game.players[0], game.getCurrentPlayer())
+        assertEquals(1, game.players[0].hands.size)
+
+    }
 }
